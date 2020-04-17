5992116ad31e9eff1dda893a779283f970c14afc
Cate Karangi <cathy.pattie@gmail.com>
2020-04-16 22:38:29 -0500

    updated

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 
 window.addEventListener("load", function() {
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(planets) {
+         console.log(planets);
+         let index = Math.floor(Math.random() * planets.length)
+         document.getElementById("missionTarget").innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${planets[index].name}</li>
+               <li>Diameter: ${planets[index].diameter}</li>
+               <li>Star: ${planets[index].star}</li>
+               <li>Distance from Earth: ${planets[index].distance}</li>
+               <li>Number of Moons: ${planets[index].moons}</li>
+            </ol>
+            <img src="${planets[index].image}">
+         `
+      })
+   })
+
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       let pilotName = document.querySelector("input[name=pilotName]");
@@ -32,12 +50,11 @@ window.addEventListener("load", function() {
       
       }
 
-      let faultyItems = document.getElementById("faultyItems");
       if(cargoMass.value > 10000){
          faultyItems.style.visibility =  "visible";
          document.getElementById("pilotStatus").innerText = `Pilot ${pilotName.value} is ready for launch.`
          document.getElementById("copilotStatus").innerText = `Co-pilot ${copilotName.value} is ready for launch.`
-         document.getElementById("fuelStatus").innerText = `Too much mass for the shuttle `
+         document.getElementById("cargoStatus").innerText = `Too much mass for the shuttle `
          document.getElementById("launchStatus").innerText = `Shuttle not ready for launch`
          document.getElementById("launchStatus").style.color = "red"
 
@@ -55,23 +72,7 @@ window.addEventListener("load", function() {
 // event.preventDefault()
 
 
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-         response.json().then( function(planets) {
-            console.log(planets);
-            let index = Math.floor(Math.random() * planets.length)
-            document.getElementById("missionTarget").innerHTML = `
-               <h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${planets[index].name}</li>
-                  <li>Diameter: ${planets[index].diameter}</li>
-                  <li>Star: ${planets[index].star}</li>
-                  <li>Distance from Earth: ${planets[index].distance}</li>
-                  <li>Number of Moons: ${planets[index].moons}</li>
-               </ol>
-               <img src="${planets[index].image}">
-            `
-         })
-      })
+ 
       event.preventDefault()
    })
    
